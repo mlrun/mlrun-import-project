b8d2ade2a4d20af12d91c8ff97bc49da4f321283
MichaelSkralivetsky <michael_skralivetsky@mckinsey.com>
2024-08-19 15:46:43 +0300

    custom_evidently_app_v2.py: make constant artifact name due to ML-7347

--- custom_evidently_app_v2.py
+++ custom_evidently_app_v2.py
@@ -55,12 +55,14 @@ class CustomEvidentlyMonitoringApp(EvidentlyModelMonitoringApplicationBaseV2):
         self.log_evidently_object(
             monitoring_context,
             data_drift_report,
-            f"report_{str(monitoring_context.end_infer_time)}",
+            "report_evidently",  # TODO: ML-7347
+            # f"report_{str(monitoring_context.end_infer_time)}",
         )
         self.log_evidently_object(
             monitoring_context,
             data_drift_test_suite,
-            f"suite_{str(monitoring_context.end_infer_time)}",
+            "suite_evidently",  # TODO: ML-7347
+            # f"suite_{str(monitoring_context.end_infer_time)}",
         )
         # TODO: commented out due to ML-7159 - evidently app pod memory consumption is constantly growing
         # self.log_project_dashboard(
